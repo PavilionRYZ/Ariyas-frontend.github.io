5e540d00b1fd8e396e3c9a490e60779436655ab6
Subhra sundar sinha <72126523+PavilionRYZ@users.noreply.github.com>
2024-06-02 21:28:06 +0530

    update vite env

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
+/* eslint-disable no-unused-vars */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target:'https://ariyas-e-commerce.onrender.com',
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: import.meta.env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+        }
       }
-    }
-  },
-  plugins: [react()],
+    },
+    plugins: [react()]
+  }
 })
